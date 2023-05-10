7e6290b8f67034f2266408a2f079818718aa15b1
zeuz <zeuz@zeuzs-iMac.local>
2023-05-10 14:46:11 -0400

    tutor helped make it work

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import inquirer from 'inquirer';
+import { writeFile } from 'fs';
+import Circle from "./lib/circle.js"
+import Triangle from "./lib/triangle.js"
+import Square from "./lib/square.js"
+import SVG from './lib/svg.js';
 
-
-var questions = [
+inquirer.prompt([
     {
         type: "input",
         name: "text",
@@ -28,4 +33,30 @@ var questions = [
         message: "What color would you like your shape to be? (color keyword OR a hexadecimal number)",
     },
 
-]
+]).then((data) => {
+    let shape;
+    if(data.shape === "Circle") {
+        shape = new Circle()
+       
+    }
+    if(data.shape === "Triangle") {
+        shape = new Triangle()
+       
+    }
+    if(data.shape === "Square") {
+        shape = new Square()
+        
+    }
+    shape.setColor(data.shapeColor)
+
+    const svg = new SVG()
+    svg.setText(data.text, data.textColor)
+    svg.setShape(shape)
+    const svgPath = `./examples/${data.shape}.svg`;
+
+    //Generate the svg logo here.
+    return writeFile(svgPath, svg.render(), (err) =>
+      err ? console.error(err) : console.log('Generated logo.svg')
+    )
+  })
+  .catch((err) => console.error(err));
